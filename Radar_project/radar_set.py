0b5bec3eb8d7771ebc15c85ac9371d13627eab56
Chotaro-0322 <bq17088@shibaura-it.ac.jp>
2021-06-07 02:55:44 +0900

    setPID

--- Radar_project/radar_set.py
+++ Radar_project/radar_set.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import carla
 from carla import Transform, Location, Rotation
+from agents.navigation.controller import VehiclePIDController
 
 import random
 import time
@@ -9,6 +10,7 @@ import math
 from multiprocessing import Process
 import threading
 import pcl
+# import pdb
 
 class VehicleGenerator():
     def __init__(self, world, map, blueprint_library, number_vehicle):
@@ -32,6 +34,7 @@ class VehicleGenerator():
         position_list = []
         self.vehicle_actor_list = []
         self.rad_actor_list = []
+        self.controller_list = []
         for n in range(self.number_vehicle):
             # spawn_point = carla.Transform()
             # spawn_point.location = self.world.get_random_location_from_navigation()
@@ -40,12 +43,14 @@ class VehicleGenerator():
             position_list.append(vehicle_transform)
         num_lost_vehicle = 0
         for n, position in enumerate(position_list):
-            print("position is ", position)
+            # print("position is ", position)
             try:
                 self.vehicle_bp = random.choice(self.blueprint_library.filter("vehicle.**.*"))
                 self.vehicle_actor = self.world.spawn_actor(self.vehicle_bp, position)
+                self.custom_controller = VehiclePIDController(self.vehicle_actor, args_lateral = {'K_P': 1, 'K_D': 0.0, 'K_I': 0}, args_longitudinal = {'K_P': 1, 'K_D': 0.0, 'K_I': 0.0})
                 self.rad_ego = self.world.spawn_actor(self.radar_bp, self.rad_transform, attach_to=self.vehicle_actor, attachment_type=carla.AttachmentType.Rigid)
                 self.vehicle_actor_list.append(self.vehicle_actor)
+                self.controller_list.append(self.custom_controller)
                 self.rad_actor_list.append(self.rad_ego)
 
                 # 最初は全て衝突していない
@@ -56,7 +61,7 @@ class VehicleGenerator():
                 num_lost_vehicle += 1
                 pass
             #self.vehicle_actor.set_autopilot(True)
-        print("車の状態は", self.vehicle_status)
+        # print("車の状態は", self.vehicle_status)
 
 
         if self.number_vehicle == 1:
@@ -66,68 +71,62 @@ class VehicleGenerator():
 
     def vehicle_transport(self):
         # print("vehicle num is ", vehicle_num, "collision is ", collision)
+        # print("vehice status is ", self.vehicle_status)
+        # print("check !!!!!")
         for i, vehicle in enumerate(self.vehicle_actor_list):
             if self.vehicle_status[str(i)] == False:
-                # print("vehicle num is ", vehicle_num, "collision is ", collision)
                 waypoint = self.map.get_waypoint(vehicle.get_location())
-                waypoint = random.choice(waypoint.next(0.2))
-                vehicle.set_transform(waypoint.transform)
-                # vehicle.set_transform()
-                # vehicle.set_location(carla.Location(x=vehicle.get_location().x, y=vehicle.get_location().y ,z=0))
-                # vehicle.set_location(carla.Location(z=0))
+                waypoint = random.choice(waypoint.next(5))
+                controll_signal = self.controller_list[i].run_step(20, waypoint)
+                self.vehicle_actor_list[i].apply_control(controll_signal)
             else:
-                # waypoint = self.map.get_waypoint(vehicle.get_location())
-                # waypoint = random.choice(waypoint.next(0.0001))
-                # vehicle.set_transform(waypoint.transform)
-                # vehicle.set_location(carla.Location(x=vehicle.get_location().x, y=vehicle.get_location().y ,z=0))
-                # print("i ", i, "vehivle is stop")
-                continue
-        
+                controll_signal = carla.VehicleControl(brake=1.0)
+                self.vehicle_actor_list[i].apply_control(controll_signal)
 
     def vehicle_radar_callback(self, vehicle_num, radar_data):
         velocity_range = 7.5 # m/s
         current_rot = radar_data.transform.rotation
         collision_bool = []
         # print("vehicle_num is ", vehicle_num)
-        print("radar_data is ", radar_data, "vehicle_num is ", vehicle_num)
+        # print("radar_data is ", radar_data, "vehicle_num is ", vehicle_num)
         for detect in radar_data:
-            print("detect is ", detect)
+            # print("detect is ", dete fst)
             azi = math.degrees(detect.azimuth)
             alt = math.degrees(detect.altitude)
             # The 0.25 adjusts a bit the distance so the dots can
             # be properly seen
-            if (detect.depth < 50) and (-10 < azi < 10) and (-10 < alt < 10):
+            if (detect.depth < 3) and (-10 < azi < 10) and (-10 < alt < 10):
                 collision_bool.append(True)
             else:
-                pass
+                continue
 
-            fw_vec = carla.Vector3D(x=detect.depth - 0.25)
+            # fw_vec = carla.Vector3D(x=detect.depth - 0.25)
             # if (detect.velocity != 0) & (azi < 0):
             #     print("detect : velocity", detect.velocity, "destance", detect.depth, "azi", azi)
-            carla.Transform(
-                carla.Location(),
-                carla.Rotation(
-                    pitch=current_rot.pitch + alt,
-                    yaw=current_rot.yaw + azi,
-                    roll=current_rot.roll)).transform(fw_vec)
+            # carla.Transform(
+            #     carla.Location(),
+            #     carla.Rotation(
+            #         pitch=current_rot.pitch + alt,
+            #         yaw=current_rot.yaw + azi,
+            #         roll=current_rot.roll)).transform(fw_vec)
 
-            def clamp(min_v, max_v, value):
-                return max(min_v, min(value, max_v))
+            # def clamp(min_v, max_v, value):
+            #     return max(min_v, min(value, max_v))
 
-            norm_velocity = detect.velocity / velocity_range # range [-1, 1]
-            r = int(clamp(0.0, 1.0, 1.0 - norm_velocity) * 255.0)
-            g = int(clamp(0.0, 1.0, 1.0 - abs(norm_velocity)) * 255.0)
-            b = int(abs(clamp(- 1.0, 0.0, - 1.0 - norm_velocity)) * 255.0)
-            #print("draw point is ", radar_data.transform.location)
-            # print("radar location is ", (radar_data.transform.location + fw_vec))
-            self.world.debug.draw_point(
-                radar_data.transform.location + fw_vec,
-                size=0.075,
-                life_time=0.06,
-                persistent_lines=False,
-                color=carla.Color(r, g, b))
+            # norm_velocity = detect.velocity / velocity_range # range [-1, 1]
+            # r = int(clamp(0.0, 1.0, 1.0 - norm_velocity) * 255.0)
+            # g = int(clamp(0.0, 1.0, 1.0 - abs(norm_velocity)) * 255.0)
+            # b = int(abs(clamp(- 1.0, 0.0, - 1.0 - norm_velocity)) * 255.0)
+            # #print("draw point is ", radar_data.transform.location)
+            # # print("radar location is ", (radar_data.transform.location + fw_vec))
+            # self.world.debug.draw_point(
+            #     radar_data.transform.location + fw_vec,
+            #     size=0.075,
+            #     life_time=0.06,
+            #     persistent_lines=False,
+            #     color=carla.Color(r, g, b))
 
-        if len(collision_bool) > 500:
+        if len(collision_bool) > 10:
             # print("vehicle__num", vehicle_num ,"detect depth ", detect.depth)
             self.vehicle_status[str(vehicle_num)] = True
         else:
@@ -271,18 +270,23 @@ class World():
     # def radar_clutering(self, radar_position):
     #     pointcloud = pcl.Pointclouod():
 
+    # def carlaEventLoop(self, world):
 
     def update(self):
         self.rad_ego.listen(lambda radar_data: self.rad_callback(radar_data))
-        GestThread = threading.Thread(target=self.GestVehicle.radar_listen())
-        GestThread.start()
-        # MainThread = threading.Thread(target=self.MainVehicle.radar_listen())
-        # MainThread.start()
+        # WorldThread = threading.Thread(target=self.carlaEventLoop, args=[self.world], daemon=True)
+        # WorldThread.start()
+        # pdb.set_trace()
         while True:
+            # self.GestVehicle.radar_listen()
             self.GestVehicle.vehicle_transport()
-            # self.MainVehicle.vehicle_transport()
             time.sleep(0.001)
 
+            # self.world.tick()
+        # GestThread = threading.Thread(target=self.GestVehicle.radar_listen())
+        # GestThread.start()
+        # MainThread = threading.Thread(target=self.MainVehicle.radar_listen())
+        # MainThread.start()
 
 if __name__ == "__main__":
     World = World()
